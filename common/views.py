b17e0f2771dd195b85dc6617db4ac1ff4e2a399d
kingoneju <parkhajh0803@naver.com>
2025-02-23 18:40:01 +0900

    error404

--- common/views.py
+++ common/views.py
@@ -22,5 +22,5 @@ def signup(request):
     return render(request, 'common/signup.html', {'form': form})
 
 def page_not_found(request, exception):
-    return render(request, 'common/404.html', status=404)
+    return render(request, 'common/404.html', {})
 
